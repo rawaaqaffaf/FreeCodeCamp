b72e4f24c5a7f3c40633d025848ec4817fbcdae1
Rawa <rawa.qaffaf@gmail.com>
2018-06-05 19:46:45 +0300

    web design

--- Wikipedia/wikipedia.js
+++ Wikipedia/wikipedia.js
@@ -1,8 +1,39 @@
+$(document).ready(function() {
 var searchterm;
 
-console.log("hello, not sure why this is not working :s");
+var data = {
+   action: "render", 
+   prop:"revisions",
+   rvprop : "content",
+   format : "jsonfm",
+   formatversion : "2",
+};
 
 
-$("#random-article").on ("click", function() {
-   $("#random-article").attr('https://en.wikipedia.org/wiki/Special:Random', "_blank");
+$("#random-article").on("click", function() {
+   $(this).attr("href", "https://en.wikipedia.org/wiki/Special:Random");
+   $(this).attr("target" , "_blank");
  });
+
+
+$("#wikisearch").on("submit", function(e) {
+	e.preventDefault();
+	searchresult();
+   console.log("this is the search term!");
+   console.log(searchterm);
+   
+
+ });
+ 
+
+ function searchresult(){
+searchterm = $('#search-term').val();
+
+ $.getJSON("http://en.wikipedia.org/w/api.php?titles=" + searchterm , data, function(jd) {
+  $("#search-result").html('<p>' + searchterm  + '</p>');
+   });
+
+
+
+ }
+});
